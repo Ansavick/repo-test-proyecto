ee30cb61b910877e1acef43972c2f6403ec88133
sabri <sabriinaveiga@gmail.com>
2024-04-15 21:53:07 -0300

    agregar configuracion para envio de mail

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -63,6 +63,7 @@ public class Main {
         properties.put("mail.smtp.starttls.enable", "true");
         properties.put("mail.smtp.host", "smtp.gmail.com");
         properties.put("mail.smtp.port", "587");
+        properties.put("mail.smtp.ssl.trust", "smtp.gmail.com");
 
 
         try {
